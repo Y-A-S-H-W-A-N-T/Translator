22e5f5c8c5b3fbad40b5af08e585af56fe9ca134
raoyashwant <raoyashwant132@gmail.com>
2023-12-19 02:29:04 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition'
 
 function App() {
 
+  const [transalation,setTranslation] = useState()
 
   const {
     transcript,
@@ -13,8 +15,33 @@ function App() {
 
   if(!browserSupportsSpeechRecognition)return 'ERROR'
 
-  const Speak = ()=>{
-    SpeechRecognition.startListening()
+  const Speak = async()=>{
+    await SpeechRecognition.startListening()
+  }
+
+
+  const Convert = async()=>{
+    const url = 'https://google-translate113.p.rapidapi.com/api/v1/translator/text';
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': 'd3a19c7887msh864b0e0b9056390p1e5a2djsn509fc8b630ba',
+        'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
+      },
+      body: new URLSearchParams({
+        from: 'auto',
+        to: 'en',
+        text: transcript
+      })
+    };
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setTranslation(result.trans);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
@@ -23,10 +50,16 @@ function App() {
           {listening? 'on' : 'off'}
         </div>
         <div className='text'>
-            {transcript}
+            <h1>{transcript}</h1>
           </div>
         <div className='mic'>
           <img src='microphone.png' alt='mic' height={50} width={50} onClick={Speak}/>
+        </div>
+        <div className='text'>
+          <button onClick={Convert}>Convert</button>
+        </div>
+        <div className='text'>
+            <h1>{transalation}</h1>
         </div>
         
     </div>
