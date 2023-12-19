c163add980d98c8b5f0244ae9111b7e645c54b51
Y-A-S-H-W-A-N-T <raoyashwant132@gmail.com>
2023-12-19 23:29:59 +0530

    to English

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition'
 function App() {
 
   const [transalation,setTranslation] = useState()
+  const [language,setLanguage] = useState('ur')
+  const [en,setEn] = useState()
 
   const {
     transcript,
@@ -31,16 +33,39 @@ function App() {
       },
       body: new URLSearchParams({
         from: 'auto',
-        to: 'en',
+        to: language,
         text: transcript
       })
-    };
+    }
     try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      setTranslation(result.trans);
+      const response = await fetch(url, options) // actual conversion
+      const result = await response.json()
+      setTranslation(result.trans)      
     } catch (error) {
-      console.error(error);
+      console.error(error)
+    }
+  }
+  const English_Conversion = async()=>{
+    const url = 'https://google-translate113.p.rapidapi.com/api/v1/translator/text';
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': 'd3a19c7887msh864b0e0b9056390p1e5a2djsn509fc8b630ba',
+        'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
+      },
+      body: new URLSearchParams({
+        from: 'auto',
+        to: 'en',
+        text: transalation
+      })
+    }
+    try {
+      const response = await fetch(url, options) // for converting response to English
+      const result = await response.json()
+      setEn(result.trans)
+    } catch (error) {
+      console.error(error)
     }
   }
 
@@ -60,6 +85,12 @@ function App() {
         </div>
         <div className='text'>
             <h1>{transalation}</h1>
+        </div>
+        <div className='text'>
+          <button onClick={English_Conversion}>To English</button>
+        </div>
+        <div className='text'>
+            <h1>{en}</h1>
         </div>
         
     </div>
